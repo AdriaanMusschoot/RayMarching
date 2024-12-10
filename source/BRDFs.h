89cd19f968324bafd4d50eb177138dc3207bb63b
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-12-10 17:28:16 +0100

    rm old comments

--- source/BRDFs.h
+++ source/BRDFs.h
@@ -8,35 +8,18 @@ namespace VM
 {
 	namespace BRDF
 	{
-		/**
-		 * \param kd Diffuse Reflection Coefficient
-		 * \param cd Diffuse Color
-		 * \return Lambert Diffuse Color
-		 */
-		static VM::ColorRGB Lambert(float kd, const VM::ColorRGB& cd)
+		static ColorRGB Lambert(float kd, const VM::ColorRGB& cd)
 		{
-			//todo: W3
 			return { cd * kd / std::numbers::pi_v<float> };
 		}
 
-		static VM::ColorRGB Lambert(const VM::ColorRGB& kd, const VM::ColorRGB& cd)
+		static ColorRGB Lambert(const VM::ColorRGB& kd, const VM::ColorRGB& cd)
 		{
-			//todo: W3
 			return { cd * kd / std::numbers::pi_v<float> };
 		}
 
-		/**
-		 * \brief todo
-		 * \param ks Specular Reflection Coefficient
-		 * \param exp Phong Exponent
-		 * \param l Incoming (incident) Light Direction
-		 * \param v View Direction
-		 * \param n Normal of the Surface
-		 * \return Phong Specular Color
-		 */
 		static VM::ColorRGB Phong(float ks, float exp, const Vector3& l, const Vector3& v, const Vector3& n)
 		{
-			//todo: W3
 			const Vector3 reflectionVector{ Vector3::Reflect(l, n)};
 			float cosAngle{ Vector3::Dot(reflectionVector, v) };
 			if (cosAngle < 0) cosAngle = 0;
@@ -44,69 +27,35 @@ namespace VM
 			return { phongSpecularReflection, phongSpecularReflection, phongSpecularReflection };
 		}
 
-		/**
-		 * \brief BRDF Fresnel Function >> Schlick
-		 * \param h Normalized Halfvector between View and Light directions
-		 * \param v Normalized View direction
-		 * \param f0 Base reflectivity of a surface based on IOR (Indices Of Refrection), this is different for Dielectrics (Non-Metal) and Conductors (Metal)
-		 * \return
-		 */
 		static VM::ColorRGB FresnelFunction_Schlick(const Vector3& h, const Vector3& v, const VM::ColorRGB& f0)
 		{
-			//todo: W3
 			float angle{ Vector3::Dot(h, v) };
 			if (angle < 0) angle = 0;
 			return f0 + (VM::ColorRGB{ 1, 1, 1 } - f0) * ((1 - angle) * (1 - angle) * (1 - angle) * (1 - angle) * (1 - angle));
 		}
 
-		/**
-		 * \brief BRDF NormalDistribution >> Trowbridge-Reitz GGX (UE4 implemetation - squared(roughness))
-		 * \param n Surface normal
-		 * \param h Normalized half vector
-		 * \param roughness Roughness of the material
-		 * \return BRDF Normal Distribution Term using Trowbridge-Reitz GGX
-		 */
 		static float NormalDistribution_GGX(const Vector3& n, const Vector3& h, float roughness)
 		{
-			//todo: W3
 			float roughnessSquared{					roughness * roughness };
 			float normalDistribution{	Vector3::Dot(n, h) * Vector3::Dot(n, h)
 										* ((roughnessSquared * roughnessSquared) - 1.f)
 										+ 1.f };
-			return { roughnessSquared * roughnessSquared / (std::numbers::pi_v<float> * normalDistribution * normalDistribution) };
+			return roughnessSquared * roughnessSquared / (std::numbers::pi_v<float> * normalDistribution * normalDistribution);
 		}
 
-
-		/**
-		 * \brief BRDF Geometry Function >> Schlick GGX (Direct Lighting + UE4 implementation - squared(roughness))
-		 * \param n Normal of the surface
-		 * \param v Normalized view direction
-		 * \param roughness Roughness of the material
-		 * \return BRDF Geometry Term using SchlickGGX
-		 */
 		static float GeometryFunction_SchlickGGX(const Vector3& n, const Vector3& v, float roughness)
 		{
-			//todo: W3
 			float roughnessSquared{ roughness * roughness };
 			float kDirect{ (roughnessSquared + 1)  * (roughnessSquared + 1) / 8.0f };
 
 			float angle{ Vector3::Dot(n, v) };
 			if (angle < 0) angle = 0;
-			return { angle / (angle * (1 - kDirect) + kDirect) };
+			return angle / (angle * (1 - kDirect) + kDirect);
 		}
 
-		/**
-		 * \brief BRDF Geometry Function >> Smith (Direct Lighting)
-		 * \param n Normal of the surface
-		 * \param v Normalized view direction
-		 * \param l Normalized light direction
-		 * \param roughness Roughness of the material
-		 * \return BRDF Geometry Term using Smith (> SchlickGGX(n,v,roughness) * SchlickGGX(n,l,roughness))
-		 */
 		static float GeometryFunction_Smith(const Vector3& n, const Vector3& v, const Vector3& l, float roughness)
 		{
-			//todo: W3
-			return { GeometryFunction_SchlickGGX(n, v, roughness) * GeometryFunction_SchlickGGX(n, l, roughness)};
+			return GeometryFunction_SchlickGGX(n, v, roughness) * GeometryFunction_SchlickGGX(n, l, roughness);
 		}
 
 	}
